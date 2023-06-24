25f0318107705c943ef4ccb6ca4b19837741a449
saumyanightwalkers <saumyas@knowledgesynonyms.com>
2023-06-24 18:13:19 +0530

    fetching wordpress post

---
+++ headlessfrontend/src/Utils.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Utils = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Utils
--- headlessfrontend/src/pages/Home.js
+++ headlessfrontend/src/pages/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const [posts,setPosts] = useState([]);
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost/headlesswordpress/wordpress/wp-json/wp/v2/posts'
+        );
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchPosts();
+  },[]);
   return (
     <div>
-      <p>Hello</p>
+      <h1>WordPress Posts</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title.rendered}</h2>
+          <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+        </div>
+      ))}
     </div>
   )
 }
